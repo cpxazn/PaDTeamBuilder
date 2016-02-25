372c27b332f82e2e9cbc25636b141ae484a95895
Daniel Luu <cpxazn@gmail.com>
2016-02-25 09:20:35 -0500

    Fixed redirect after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -362,7 +362,8 @@ class ApplicationController < ActionController::Base
   #Devise redirect back to last age
   def store_location
     # store last url as long as it isn't a /users path
-	session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/ or request.fullpath =~ /\/json/
+	session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/ or request.fullpath =~ /\/json/ or request.fullpath =~ /\/static/
+	
   end
   def after_sign_in_path_for(resource)
 	  session[:previous_url] || root_path
