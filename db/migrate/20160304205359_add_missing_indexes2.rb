ce5847fc2703c19896275e99dde09b35e7b83878
Daniel Luu <cpxazn@gmail.com>
2016-03-05 12:54:39 -0500

    Updates indexes again to remove duplicates

--- db/migrate/20160304205359_add_missing_indexes2.rb
+++ db/migrate/20160304205359_add_missing_indexes2.rb
@@ -2,12 +2,12 @@
       def change
         add_index :vote_lls, :user_id
         #add_index :votes, :leader_id
-        add_index :votes, :sub_id
-        add_index :votes, :user_id
+        #add_index :votes, :sub_id
+        #add_index :votes, :user_id
         add_index :comment_lls, :user_id
-        add_index :comments, :user_id
-        add_index :comments, :leader_id
-        add_index :comments, :sub_id
+        #add_index :comments, :user_id
+        #add_index :comments, :leader_id
+        #add_index :comments, :sub_id
         add_index :c_ll_ratings, :comment_ll_id
         add_index :c_ll_ratings, :user_id
       end
