6a7635bf75ec388f8d7a8efb394e98006a3d3e40
Daniel Luu <cpxazn@gmail.com>
2016-03-05 12:52:10 -0500

    Update migration for indexes

--- db/migrate/20160304205359_add_missing_indexes2.rb
+++ db/migrate/20160304205359_add_missing_indexes2.rb
@@ -1,7 +1,7 @@
     class AddMissingIndexes2 < ActiveRecord::Migration
       def change
         add_index :vote_lls, :user_id
-        add_index :votes, :leader_id
+        #add_index :votes, :leader_id
         add_index :votes, :sub_id
         add_index :votes, :user_id
         add_index :comment_lls, :user_id
